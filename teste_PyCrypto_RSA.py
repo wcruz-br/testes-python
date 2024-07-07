3feed712e8cc461270f17301126ba00fdb1656c5
wcruz-br <wcruz.br@gmail.com>
2024-07-07 17:18:41 -0300

    teste criptografia RSA

--- teste_PyCrypto_RSA.py
+++ teste_PyCrypto_RSA.py
@@ -1,4 +1,27 @@
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 key = RSA.generate(2048)
-private_key = key.exportKey()
-public_key = key.publickey().exportKey()
+private_key = key
+public_key = key.publickey()
+private_key_export = key.exportKey()
+public_key_export = key.publickey().exportKey()
+
+# print('Private Key:')
+# print(private_key)
+# print
+# print('Public Key:')
+# print(public_key)
+
+encryptor = PKCS1_OAEP.new(public_key)
+encrypted = encryptor.encrypt(b'Mensagem secreta')
+
+print('-----------------------------------------------------------------')
+print(encrypted.hex())
+print('-----------------------------------------------------------------')
+
+decryptor = PKCS1_OAEP.new(private_key)
+decrypted = decryptor.decrypt(encrypted)
+
+print(decrypted.decode('utf-8'))
+print('-----------------------------------------------------------------')

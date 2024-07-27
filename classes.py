f89892e46cd57ada77da783aaa9b37e13db67c59
wcruz-br <wcruz.br@gmail.com>
2024-07-27 20:24:34 -0300

    minor changes

---
+++ classes.py
@@ -0,0 +1,19 @@
+import math
+def circle_area(radius):
+    return math.pi * pow(radius, 2)
+
+circle = {
+    "radius": 3,
+    "color": "blue"
+}
+
+# print(circle_area(circle["radius"]))
+
+person = {
+    "first": "Willian",
+    "last": "Cruz",
+}
+
+print(person["first"] + " " + person["last"])
+
+
